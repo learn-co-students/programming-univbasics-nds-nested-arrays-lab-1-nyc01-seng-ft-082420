002fc16c4c1acfff37ed3d1344b0eaa454267fa0
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-06 01:57:56 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,6 +17,9 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
+  all_produce = [
+    CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE
+  ]
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
@@ -24,18 +27,25 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
+  # Using Array literal syntax only, build another nested array that
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
+  all_produce = [
+    CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort
+  ]
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
+  # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
+  matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
+  # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+matrix[0][2]
+matrix[0][2] = new_value
+matrix
 end
